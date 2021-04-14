bd8406b4106f6b50bd6debf9acdf308cccc23b2d
carloteran19 <carloteran19@gmail.com>
2021-04-14 12:48:32 -0500

    Problem Solving Patterns

---
+++ problem-solving-patterns/frequency-counter/anagram-challenge.js
@@ -0,0 +1,16 @@
+// Anagrams
+
+// Given two strings, write a function to determine if the second string in as anagram of the first.
+// An anagram is a word, phrase or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+function validAnagram(str, str2){
+    // write your code here
+}
+
+
+// Tests
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') //true
+validAnagram('rat', 'car') // false
+validAnagram('awesome', 'awesom') //false
